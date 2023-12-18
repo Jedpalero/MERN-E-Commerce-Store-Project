8fd07d676c3c90b58352356e7cb8182543434da1
Jed Palero <raven@developer.dev>
2023-12-18 16:42:15 +0800

    Designing Header & Product Screens in frontend

---
+++ frontend/src/pages/Products/HeartIcon.jsx
@@ -0,0 +1,51 @@
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addToFavorites,
+  removeFromFavorites,
+  setFavorites,
+} from "../../redux/features/favorites/favoriteSlice";
+import {
+  addToFavoriteToLocalStorage,
+  getFavoritesFromLocalStorage,
+  removeFavoriteFromLocalStorage,
+} from "../../Utils/localStorage";
+import { useEffect } from "react";
+
+const HeartIcon = ({ product }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites) || [];
+  const isFavorite = favorites.some((p) => p._id === product._id);
+
+  useEffect(() => {
+    const favoritesFromLocalStorage = getFavoritesFromLocalStorage();
+    dispatch(setFavorites(favoritesFromLocalStorage));
+  }, []);
+
+  const toggleFavorites = () => {
+    if (isFavorite) {
+      dispatch(removeFromFavorites(product));
+      // remove the product from the localStorage as well
+      removeFavoriteFromLocalStorage(product._id);
+    } else {
+      dispatch(addToFavorites(product));
+      // add the product to localStorage as well
+      addToFavoriteToLocalStorage(product);
+    }
+  };
+
+  return (
+    <div
+      onClick={toggleFavorites}
+      className="absolute top-2 right-5 cursor-pointer"
+    >
+      {isFavorite ? (
+        <FaHeart className="text-pink-500" />
+      ) : (
+        <FaRegHeart className="text-white" />
+      )}
+    </div>
+  );
+};
+
+export default HeartIcon;
