86a3c1f9d1debeae692eefb2e546be57fea4fded
Jed Palero <raven@developer.dev>
2023-12-20 12:04:19 +0800

    Order Management & Secure Payments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -28,6 +28,13 @@ import ProductDetails from "./pages/Products/ProductDetails.jsx";
 
 import Cart from "./pages/Cart.jsx";
 import Shop from "./pages/Shop.jsx";
+import Shipping from "./pages/Orders/Shipping.jsx";
+import PlaceOrder from "./pages/Orders/PlaceOrder.jsx";
+
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import Order from "./pages/Orders/Order.jsx";
+import UserOrder from "./pages/User/UserOrder.jsx";
+import Orderlist from "./pages/Admin/Orderlist.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -39,9 +46,13 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/user-orders" element={<UserOrder />} />
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/order/:id" element={<Order />} />
       </Route>
 
       {/* Admin Routes */}
@@ -50,6 +61,7 @@ const router = createBrowserRouter(
         <Route path="categorylist" element={<CategoryList />} />
         <Route path="productlist/" element={<ProductList />} />
         <Route path="allproductlist" element={<AllProducts />} />
+        <Route path="orderlist" element={<Orderlist />} />
         <Route path="product/update/:_id" element={<ProductUpdate />} />
       </Route>
     </Route>
@@ -58,6 +70,8 @@ const router = createBrowserRouter(
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PayPalScriptProvider>
+      <RouterProvider router={router} />
+    </PayPalScriptProvider>
   </Provider>
 );
